b6ddd4b4aa6eab49bc9f00efb946939121a77014
Tomasz Miśków <miskow.tomasz@gmail.com>
2019-10-30 22:50:40 +0100

    Add scheduling cli command

--- main.py
+++ main.py
@@ -1,17 +1,38 @@
+import time
 from multiprocessing.pool import Pool
 from typing import List
 
 import click
+from click import Context
 from tqdm import tqdm
 
 from src.database import initialize_database, Computer
 from src.ping import ping_hosts
 
 
-@click.command()
+@click.group()
+def cli():
+    pass
+
+
+@cli.command()
+@click.option('--interval', 'interval', type=int, default=60, help='Sleep interval length.')
+@click.option('--threads', 'thread_count', type=int, default=1, help='Number of threads.')
+@click.pass_context
+def schedule(ctx: Context, interval: int, thread_count: int):
+    while True:
+        ctx.invoke(run, thread_count=thread_count)
+        sleep_with_tqdm(interval)
+
+
+def sleep_with_tqdm(seconds: int):
+    for _ in tqdm(range(seconds), desc='Sleeping'):
+        time.sleep(1)
+
+@cli.command()
 @click.option('--threads', 'thread_count', type=int, default=1, help='Number of threads')
-@click.option('--database', 'database_path', type=str, default='database.db', help='Database path')
-def run(database_path, thread_count):
+def run(thread_count):
+    database_path = 'database.db'
     session = initialize_database(database_path)
     computers = session.query(Computer)
     host_names = [computer.id for computer in computers]
@@ -21,7 +42,7 @@ def run(database_path, thread_count):
     session.commit()
 
 
-def ping_hosts_with_tqdm(pool: Pool, host_names: List[str]):
+def ping_hosts_with_tqdm(pool: Pool, host_names: List[str]) -> tqdm:
     return tqdm(
         ping_hosts(pool, host_names),
         desc='Pinging hosts',
@@ -31,4 +52,4 @@ def ping_hosts_with_tqdm(pool: Pool, host_names: List[str]):
 
 
 if __name__ == '__main__':
-    run()
+    cli()
